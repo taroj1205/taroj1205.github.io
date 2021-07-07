ab9bf9308b83e59724ea3801e91d4d09996dfbe4
taroj1205 <taroj1205@gmail.com>
2021-07-07 15:48:33 +1200

    First Commit
    Added the index page and their script and style file. Started creating newtab page.

---
+++ script.js
@@ -0,0 +1,9 @@
+function search() {
+  var input = document.getElementById('searchinput').value;
+  window.open("https://google.com/search?q="+input+" site:taroj1205.github.com");
+}
+
+function google() {
+  var input = document.getElementById('searchinput').value;
+  window.open("https://google.com/search?q="+input);
+}
