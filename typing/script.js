55da811b6dcb9e8660d88c067f5c20c7926b2ae9
taroj1205 <taroj1205@gmail.com>
2023-02-14 15:40:12 +1300

    now dupes instead of stacking previously typed words

--- typing/script.js
+++ typing/script.js
@@ -50,56 +50,64 @@ form.addEventListener('submit', (event) => {
     checkCredentials(username, password);
 });
 
+let num = 0;
+let en = null;
+let ja = null;
 
 function start() {
-    let num = 0;
     const request = new XMLHttpRequest();
-    request.open("GET", "https://gist.githubusercontent.com/taroj1205/420c2e76184a47b18543c52ba229f510/raw/adcef62cf11593879be2ed1d715daeeca9bda7e5/dictionary.csv", true);
+    request.open("GET", "https://raw.githubusercontent.com/taroj1205/taroj1205.github.io/main/typing/Files/dictionary.csv", true);
     request.onreadystatechange = function() {
         if (this.readyState === 4 && this.status === 200) {
             const lines = this.responseText.split("\n");
             const randomLine = lines[Math.floor(Math.random() * lines.length)];
-            const [en, ja] = randomLine.split(",");
-            document.querySelector("#ja").innerHTML = ja;
-            document.querySelector("#en").innerHTML = "<span style='color: white;'>" + en + "</span>";
-            document.getElementById("game").style.display = "block";
-            document.getElementById("startMenu").style.display = "none";
-            document.getElementById("player").style.display = "block";
-            document.getElementById("history").style.display = "block";
+            [en, ja] = randomLine.split(",");
 
+            updateUI();
             getData();
-
-            document.addEventListener("keypress", function(event) {
-                let key = event.key;
-                console.log(num,en[num],en,ja);
-
-                if (num < en.length)
-                {
-                    if (key === en[num]) {
-                        num++;
-                        const typedOut = "<span style='color: grey;' id='typedOut'>" + en.substring(0, num) + "</span>";
-                        const notYet = "<span style='color: #1fd755;' id='notYet'>" + en.substring(num) + "</span>";
-                        document.querySelector("#en").innerHTML = typedOut + notYet;
-                    }
-                    else {
-                        const typedOut = "<span style='color: grey;' id='typedOut'>" + en.substring(0, num) + "</span>";
-                        const notYet = "<span style='color: #e06c75;' id='notYet'>" + en.substring(num) + "</span>";
-                        document.querySelector("#en").innerHTML = typedOut + notYet;
-                    }
-                }
-                if (en.length <= num) {
-                    submitData(en, ja);
-                    num = 0;
-                    start();
-                }
-            });
-
-        }
-        else if (this.readyState === XMLHttpRequest.DONE && this.status !== 200) {
+            handleKeyPress();
+        } else if (this.readyState === XMLHttpRequest.DONE && this.status !== 200) {
             document.querySelector('body').innerHTML = '<h1 style="text-align: center; font-size: 10vh;">Offline contact <a href="https://twitter.com/taroj1205">@taroj1205</a></h1>';
         }
     };
     request.send();
+}
+
+function updateUI() {
+    document.querySelector("#ja").innerHTML = ja;
+    document.querySelector("#en").innerHTML = "<span style='color: white;'>" + en + "</span>";
+    document.getElementById("game").style.display = "block";
+    document.getElementById("startMenu").style.display = "none";
+    document.getElementById("player").style.display = "block";
+    document.getElementById("history").style.display = "block";
+}
+
+function handleKeyPress() {
+    document.addEventListener("keypress", function(event) {
+        let key = event.key;
+        console.log(num, en[num], en, ja);
+
+        if (num < en.length) {
+            if (key === en[num]) {
+                num++;
+                const typedOut = "<span style='color: grey;' id='typedOut'>" + en.substring(0, num) + "</span>";
+                const notYet = "<span style='color: #1fd755;' id='notYet'>" + en.substring(num) + "</span>";
+                document.querySelector("#en").innerHTML = typedOut + notYet;
+            } else {
+                const typedOut = "<span style='color: grey;' id='typedOut'>" + en.substring(0, num) + "</span>";
+                const notYet = "<span style='color: #e06c75;' id='notYet'>" + en.substring(num) + "</span>";
+                document.querySelector("#en").innerHTML = typedOut + notYet;
+            }
+        }
+
+        if (en.length === num) {
+            console.log("All typed out!");
+            submitData(en, ja);
+            num = 0;
+            console.log("num reset!");
+            start();
+        }
+    });
 }
 
 // Send data
@@ -119,6 +127,7 @@ function submitData(en, ja) {
     const password = localStorage.getItem('password');
     const data = 'username=' + encodeURIComponent(username) + '&password=' + encodeURIComponent(password) + '&en=' + encodeURIComponent(en) + '&ja=' + encodeURIComponent(ja);
     xhr.send(data);
+    console.log("submitData() complete.");
 }
 
 // Receive data
