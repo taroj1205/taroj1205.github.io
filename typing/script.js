8f1461f6c88ffdb951b169e6f468ee2f96a7f8fe
taroj1205 <taroj1205@gmail.com>
2023-03-07 20:22:03 +1300

    Change width/height based on visible width and height

--- typing/script.js
+++ typing/script.js
@@ -50,6 +50,15 @@ window.onload = function() {
     }
 };
 
+const setVisualViewport = () => {
+    const vv = window.visualViewport
+    const root = document.documentElement
+    root.style.setProperty('--vvw', `${vv.width}px`)
+    root.style.setProperty('--vvh', `${vv.height}px`)
+}
+setVisualViewport()
+window.visualViewport.addEventListener('resize', setVisualViewport)
+
 // Check if the username and password match a stored database
 function checkCredentials(username, password) {
     let xhr = new XMLHttpRequest();
